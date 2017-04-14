559a599a266a5bcd0acbe4c365cd8cac0bfe5aba
Sophia ROLAND <soroland@adneom.com>
2017-04-14 09:11:59 +0100

    feat(scrollbar): added scrollbar (doesn't work with universal so far)

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'; // needed for ngIf, ngFor, etc.
 import { Routes, Router, RouterModule, ActivatedRoute } from '@angular/router';
 import { ArticleListComponent, ArticleItemComponent } from './index';
 import { ArticlesService } from './articles.service';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 @NgModule({
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, PerfectScrollbarModule],
   exports: [
     ArticleListComponent,
     ArticleItemComponent
